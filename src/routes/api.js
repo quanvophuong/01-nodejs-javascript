af2ecdbc13f46cb2b4102ce68c605f4015fb2ace
Quan Vo <33823448+quanvophuong@users.noreply.github.com>
2024-08-16 16:33:44 +0700

    step 1

---
+++ src/routes/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const routerAPI = express.Router();
+
+// const { getUsersAPI, postCreateUserAPI,
+//     putUpdateUserAPI, deleteUserAPI
+
+// } = require('../controllers/apiController')
+
+
+// routerAPI.get('/users', getUsersAPI);
+// routerAPI.post('/users', postCreateUserAPI);
+// routerAPI.put('/users', putUpdateUserAPI);
+// routerAPI.delete('/users', deleteUserAPI);
+
+routerAPI.get('/', (req, res) => {
+    res.status(200).json('Hello World!');
+});
+
+
+
+module.exports = routerAPI; //export default
