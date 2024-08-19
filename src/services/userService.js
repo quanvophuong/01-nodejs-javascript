a94190e1cb0557b375ea6962da022f72e1a228d7
Quan Vo <33823448+quanvophuong@users.noreply.github.com>
2024-08-19 16:54:58 +0700

    finish access token

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,9 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
-const createUserService = async ( name,email,password ) => {
+const createUserService = async (name, email, password) => {
     try {
         // hash user password
         const hashPassword = await bcrypt.hash(password, saltRounds);
@@ -22,6 +23,47 @@ const createUserService = async ( name,email,password ) => {
     }
 }
 
+const loginService = async (email, password) => {
+    try {
+
+        // fetch user by email
+        const user = await User.findOne({email: email});
+        if (user) {
+            // compare password
+            const isMatchPassword = await bcrypt.compare(password, user.password);
+            if (!isMatchPassword) {
+                return {
+                    EC: 2,
+                    EM: "Email/Password không hợp lệ"
+                }
+            } else {
+                // create an access token
+                const payload = {
+                    email: user.email,
+                    name: user.name
+                };
+                const access_token = jwt.sign(payload, process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXPIRE});
+                return {
+                    access_token,
+                    user: {
+                        email: user.email,
+                        name: user.name
+                    }
+                };
+            }
+        } else {
+            return {
+                EC: 1,
+                EM: "Email/Password không hợp lệ"
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
 module.exports = {
-    createUserService
+    createUserService, loginService
 }
