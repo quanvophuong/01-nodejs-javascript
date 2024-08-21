309a8434a0c05b2e849fd0253925e795c19fb46d
Quan Vo <33823448+quanvophuong@users.noreply.github.com>
2024-08-21 14:08:07 +0700

    finish khoa hoc fullstack react/nodejs

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const {createUser, handleLogin, getUser, getAccount} = require("../controllers/userController");
 const auth = require("../middleware/auth");
-const delay = require("../middleware/delay");
 
 const routerAPI = express.Router();
 
@@ -14,7 +13,7 @@ routerAPI.get('/', (req, res) => {
 routerAPI.post('/register',createUser);
 routerAPI.post('/login',handleLogin);
 routerAPI.get('/user',getUser);
-routerAPI.get('/account',delay,getAccount);
+routerAPI.get('/account',getAccount);
 
 
 
